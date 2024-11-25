a5b7ef89d3302f8999d5c9393becd32bbb534e45
samberubz <141795376+samberubz@users.noreply.github.com>
2024-11-24 19:27:33 -0500

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -99,7 +99,7 @@ st.write("")
 # 1. Slider
 st.markdown(
     """
-    <h5 style="text-align: center; padding-bottom: 0px; margin-bottom: -90px;">Select Forecast Time [6 to 48 hours]</h5>
+    <h5 style="text-align: center; padding-bottom: 15px; margin-bottom: -50px;">Select Forecast Time [6 to 48 hours]</h5>
     """,
     unsafe_allow_html=True)
 st.markdown("""
