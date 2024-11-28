8d920c6a791de9c6d9d3f7f9831a3d0a764365e8
samberubz <141795376+samberubz@users.noreply.github.com>
2024-11-28 18:04:08 -0500

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -292,15 +292,6 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-@media {
-    .tremblant-table {
-        width: 95%; /* Make the table fit smaller screens */
-    }
-    .tremblant-table th, .tremblant-table td {
-        padding: 10px; /* Reduce padding on small screens */
-        font-size: 14px; /* Adjust font size */
-    }
-}
 # 2. Display
 layer_tremblant = pdk.Layer(
     "HeatmapLayer",
