ebf71727b3737b67ec6fff8f3722ab329be2c247
samberubz <141795376+samberubz@users.noreply.github.com>
2024-11-28 19:34:44 -0500

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -257,7 +257,7 @@ st.markdown(
     .tremblant-table {{
         border-collapse: collapse;
         width: 80%; /* Increase the table width */
-        min-width: 100px; /* Ensure the table has a minimum width */
+        min-width: 30px; /* Ensure the table has a minimum width */
         margin: 20px auto; /* Center the table */
         font-family: 'Montserrat', sans-serif;
     }}
